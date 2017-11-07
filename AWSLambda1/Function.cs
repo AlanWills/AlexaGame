a0e1c826eb2008c7820258936ee79b6b350565e1
Alan Wills <alawills@googlemail.com>
2017-11-07 22:21:11 +0000

    Added mp3 file

--- AWSLambda1/Function.cs
+++ AWSLambda1/Function.cs
@@ -17,11 +17,6 @@ namespace AWSLambda1
 {
     public class Function
     {
-        //public string FunctionHandler(JSONObject input, ILambdaContext context)
-        //{
-        //    return input.StarSign + "Blob";
-        //}
-
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
@@ -30,25 +25,7 @@ namespace AWSLambda1
         /// <returns></returns>
         public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            //string starSign = (input.Request as IntentRequest).Intent.Slots["starsign"].Value.ToLower();
-            //string text = "";
-            //if (starSign == "cancer")
-            //{
-            //    text = "Good things";
-            //}
-            //else if (starSign == "aquarius")
-            //{
-            //    text = "Bad things";
-            //}
-            //else if (starSign == "leo")
-            //{
-            //    text = "Bad things";
-            //}
-
-            // Now try with google drive file - won't work though, as they're not audio files
-            // Maybe we should host on our own server?
-            // Or on github?
-            return ResponseBuilder.AudioPlayerPlay(PlayBehavior.ReplaceAll, "https://opengameart.org/sites/default/files/Mountain%20_0.mp3", "audio-token");
+            return ResponseBuilder.AudioPlayerPlay(PlayBehavior.ReplaceAll, "https://github.com/AlanWills/AlexaGame/blob/master/AWSLambda1/WELL_DONE_FLOWER.m4a", "audio-token");
         }
     }
 }
